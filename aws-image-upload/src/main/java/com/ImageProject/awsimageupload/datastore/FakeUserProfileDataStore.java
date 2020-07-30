e079c8e4538e97fbec2b6c97abe39f221e249515
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-07-30 13:11:30 +1000

    Creating user profile and a in memory database
    To start building API we created an in memory database for now.

---
+++ aws-image-upload/src/main/java/com/ImageProject/awsimageupload/datastore/FakeUserProfileDataStore.java
@@ -0,0 +1,26 @@
+package com.ImageProject.awsimageupload.datastore;
+
+
+import com.ImageProject.awsimageupload.profile.UserProfile;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class FakeUserProfileDataStore {
+
+    private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
+
+    static {
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "janetjones", null));
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "samjunior", null));
+
+    }
+
+    List<UserProfile> getUserProfiles() {
+        return USER_PROFILES;
+    }
+
+}
