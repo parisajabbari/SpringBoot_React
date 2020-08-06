e0081f55b78bcd326be0ef153a24d89561a7426f
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-08-06 15:35:33 +1000

    upload image method added
    upload image method added. just a place holder for now

--- aws-image-upload/src/main/java/com/ImageProject/awsimageupload/profile/UserProfileController.java
+++ aws-image-upload/src/main/java/com/ImageProject/awsimageupload/profile/UserProfileController.java
@@ -2,12 +2,13 @@ package com.ImageProject.awsimageupload.profile;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
@@ -23,6 +24,17 @@ public class UserProfileController {
     @GetMapping
     public List<UserProfile> getUserProfile(){
         return userProfileService.getUserProfiles();
+    }
+
+    @PostMapping(
+            path = "{userProfileId/image/upload}",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId,
+                                       @RequestParam("file") MultipartFile file){
+        userProfileService.uploadUserProfileImage(userProfileId, file);
+
     }
 
 }
